8e6cfe8248cc7dd0ecf6a9d32f6ea209af47c05c
American Idiot <jasondnettle@outlook.com>
2025-04-03 12:18:36 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/DigitalProgram/", // 👈 Fix for GitHub Pages deployment
+  base: "./", // 🔥 Fix for GitHub Pages
   server: {
     host: "::",
     port: 8080,
